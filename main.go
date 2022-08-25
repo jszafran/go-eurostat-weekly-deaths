26c060d345def3bd3a19ea71c16d8ab50077173f
jszafran <jszafran.pv@gmail.com>
2022-08-25 22:13:21 +0200

    Added logic for parsing weekly deaths data.

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"eurostat-weekly-deaths/parser"
+	"fmt"
 	"log"
+	"time"
 )
 
 func main() {
-	_, err := parser.Parse("data/weekly_deaths.tsv")
+	st := time.Now()
+	r, err := parser.Parse("data/weekly_deaths.tsv")
 	if err != nil {
 		log.Fatal(err)
 	}
+	tt := time.Since(st)
+	fmt.Printf("Parsed %d records in %v.", len(r), tt)
 }
--- parser/weekly_deaths.go
+++ parser/weekly_deaths.go
@@ -6,50 +6,118 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type Record struct {
-	MetaData
-	WeeklyDeaths
+	Age          string
+	Sex          string
+	Country      string
+	WeeklyDeaths int
+	Year         int
+	Week         int
 }
 
-type MetaData struct {
+type Demographics struct {
 	Age     string
 	Sex     string
 	Country string
 }
 
 type WeeklyDeaths struct {
-	Year  int
-	Week  int
 	Value int
 }
 
-func parseMetaData(l string) MetaData {
+type WeekInfo struct {
+	Year int
+	Week int
+}
+
+func parseDemographics(l string) Demographics {
 	spl := strings.Split(l, ",")
-	return MetaData{
+	return Demographics{
 		Age:     spl[0],
 		Sex:     spl[1],
 		Country: spl[3],
 	}
 }
 
-func parseWeeklyDeaths(recs []string) []WeeklyDeaths {
-	var wd []WeeklyDeaths
-	return wd
+func parseWeeklyDeathsValue(v string) (int, error) {
+	var vi int
+
+	v = strings.Replace(v, " ", "", -1)
+
+	if strings.Contains(v, ":") {
+		return -1, nil
+	}
+
+	if strings.Contains(v, "p") {
+		v = strings.Replace(v, "p", "", -1)
+	}
+
+	vi, err := strconv.Atoi(v)
+	if err != nil {
+		return vi, err
+	}
+	return vi, nil
 }
 
-func weekColsMap(rcs []string) map[int]string {
-	m := make(map[int]string)
+func parseWeeklyDeathsLine(rec []string) ([]int, error) {
+	wd := make([]int, len(rec))
+	for i, r := range rec {
+		v, err := parseWeeklyDeathsValue(r)
+		if err != nil {
+			return wd, err
+		}
+		wd[i] = v
+	}
+	return wd, nil
+}
 
-	for i, col := range rcs {
+func parseWeekInfo(s string) (WeekInfo, error) {
+	var wi WeekInfo
+
+	s = strings.Replace(s, " ", "", -1)
+
+	spl := strings.Split(s, "W")
+
+	if len(spl) != 2 {
+		return wi, fmt.Errorf("bad header value: %s", s)
+	}
+
+	yr, err := strconv.Atoi(spl[0])
+	if err != nil {
+		return wi, err
+	}
+
+	wk, err := strconv.Atoi(spl[1])
+	if err != nil {
+		return wi, err
+	}
+
+	return WeekInfo{Week: wk, Year: yr}, nil
+}
+
+func weekInfoColsMap(r []string) (map[int]WeekInfo, error) {
+	m := make(map[int]WeekInfo)
+
+	for i, col := range r {
 		if i == 0 {
 			continue
 		}
-		m[i] = col
+
+		wi, err := parseWeekInfo(col)
+		if err != nil {
+			return m, err
+		}
+
+		m[i] = WeekInfo{
+			Year: wi.Year,
+			Week: wi.Week,
+		}
 	}
-	return m
+	return m, nil
 }
 
 func readGzippedTSV(path string) (*csv.Reader, error) {
@@ -76,23 +144,36 @@ func Parse(path string) ([]Record, error) {
 	}
 
 	hdr, err := r.Read()
-	fmt.Println(hdr[0])
 	if err != nil {
 		return recs, err
 	}
-	//_ := weekColsMap(hdr)
+	wim, err := weekInfoColsMap(hdr)
 
 	for {
 		rec, err := r.Read()
 		if err != nil {
 			if err == io.EOF {
 				err = nil
-				fmt.Println("End of file reached!")
 				break
 			}
 		}
-		meta := parseMetaData(rec[0])
-		fmt.Printf("%+v\n", meta)
+		dmg := parseDemographics(rec[0])
+		vals, err := parseWeeklyDeathsLine(rec[1:])
+		if err != nil {
+			return recs, err
+		}
+
+		for i, v := range vals {
+			wi := wim[i+1]
+			recs = append(recs, Record{
+				Age:          dmg.Age,
+				Sex:          dmg.Sex,
+				Country:      dmg.Country,
+				WeeklyDeaths: v,
+				Year:         wi.Year,
+				Week:         wi.Week,
+			})
+		}
 	}
 
 	return recs, err
