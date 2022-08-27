a52af1a0d493f9b623d2d16978db6a85bdd9fc6e
jszafran <jszafran.pv@gmail.com>
2022-08-27 16:08:59 +0200

    Install gin + add countries static list.

---
+++ cmd/server/routes.go
@@ -0,0 +1,43 @@
+package server
+
+func ListCountries() {
+	//AD
+	//AL
+	//AM
+	//AT
+	//BE
+	//BG
+	//CH
+	//CY
+	//CZ
+	//DE
+	//DK
+	//EE
+	//EL
+	//ES
+	//FI
+	//FR
+	//GE
+	//HR
+	//HU
+	//IE
+	//IS
+	//IT
+	//LI
+	//LT
+	//LU
+	//LV
+	//ME
+	//MT
+	//NL
+	//NO
+	//PL
+	//PT
+	//RO
+	//RS
+	//SE
+	//SI
+	//SK
+	//UK
+
+}
